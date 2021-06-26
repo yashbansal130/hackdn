bb5af09035f5b7c92cbb421d21028a9fbdc3f385
yogi230 <yogesh.dhanuka.che19@itbhu.ac.in>
2021-06-26 13:06:51 +0530

    intent added

--- hackedin/app/src/main/java/com/example/hackedin/Login.java
+++ hackedin/app/src/main/java/com/example/hackedin/Login.java
@@ -22,36 +22,33 @@ public class Login extends AppCompatActivity {
     EditText inputPassword;
     Button buttonloginSubmit;
     App app;
-
+    private static final String appID = "hachedin-giiqj";
+    private static final String LOG_TAG =Login.class.getSimpleName();
+    User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         buttonloginSubmit = (Button) findViewById(R.id.login_button);
-        inputEmail = findViewById(R.id.input_email);
-        inputPassword = findViewById(R.id.input_password);
-        Realm.init(this);
-        app = new App(new AppConfiguration.Builder("application-0-aybxr").build());
+        inputEmail = findViewById(R.id.input_login_email);
+        inputPassword = findViewById(R.id.input_login_password);
+        app = new App(new AppConfiguration.Builder(appID).build());
 
         buttonloginSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loginSubmit(v);
-            }
-        });
-    }
-
-    private void loginSubmit(View view){
-        String email = inputEmail.getText().toString();
-        String password = inputPassword.getText().toString();
-        Credentials emailPasswordCredentials = Credentials.emailPassword(email, password);
-        AtomicReference<User> user = new AtomicReference<User>();
-        app.loginAsync(emailPasswordCredentials, it -> {
-            if (it.isSuccess()) {
-                Log.v("AUTH", "Successfully authenticated using an email and password.");
-                user.set(app.currentUser());
-            } else {
-                Log.e("AUTH", it.getError().toString());
+                String email = inputEmail.getText().toString();
+                String password = inputPassword.getText().toString();
+                Credentials emailPasswordCredentials = Credentials.emailPassword(email, password);
+                app.loginAsync(emailPasswordCredentials, it -> {
+                    if (it.isSuccess()) {
+                        Log.v("AUTH", "Successfully authenticated using an email and password.");
+                        user=app.currentUser();
+                        Log.v(LOG_TAG,"user id="+user);
+                    } else {
+                        Log.e(LOG_TAG, it.getError().toString());
+                    }
+                });
             }
         });
     }
